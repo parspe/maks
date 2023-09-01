468509c924106deda86a8054e610a42ec6bfcbff
parspe <143789580+parspe@users.noreply.github.com>
2023-09-01 08:34:32 +0000

    234

--- time.py
+++ time.py
@@ -7,6 +7,20 @@ def convert(seconds):
 
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
-# Driver program
-n = 12345
-print(convert(n))
+# # Driver program
+# n = 12345
+# print(convert(n))
+
+
+# def convert(seconds):
+#     seconds = seconds % (24 * 3600)
+#     hour = seconds // 3600
+#     seconds %= 3600
+#     minutes = seconds // 60
+#     seconds %= 60
+
+#     return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+# # Driver program
+# n = 12345
+# print(convert(n))
