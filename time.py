e8efccae6297c3d901d47e35d9655fc8701c9851
parspe <143789580+parspe@users.noreply.github.com>
2023-09-01 09:11:35 +0000

    23.87

--- time.py
+++ time.py
@@ -1,7 +1,7 @@
 def convert(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
-    seconds %= 3600
+    seconds %= 3600 ##seconds
     minutes = seconds // 60
     seconds %= 60
 
